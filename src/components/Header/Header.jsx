95590c1c63aade20f89fbf0dc4b1911fe6de74c1
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-30 01:20:53 +0530

    add the header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <div className="flex flex-col justify-center text-center mt-10 text-white gap-3">
+      <h1 className="text-5xl font-bold ">We Lead You To The Right Company, start the search</h1>
+      <p className="text-xl">We Offer A Job That Will Make Your Dreams Come True</p>
+    </div>
+  )
+}
+
+export default Header
