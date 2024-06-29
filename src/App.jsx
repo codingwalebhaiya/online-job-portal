b0cb2dfbb2b02d53c36177af63bbe338e61ee274
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-30 01:20:18 +0530

    add navbar component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header.jsx"
+import Navbar from "./components/Navbar/Navbar.jsx"
+import SearchBar from "./components/SearchBar/SearchBar.jsx"
+
+const App = () => {
+  return (
+   <div>
+    <Navbar/>
+    <Header/>
+    <SearchBar/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+  return (
+    <div className=" flex items-center w-full  font-serif text-white h-20 ">
+    <div className="text-3xl pl-20 font-bold">JO(BA)2</div>
+    </div>
+  )
+}
+
+export default Navbar
