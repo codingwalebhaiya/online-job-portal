bdba38ac9299fb812744e812dcf4747b8e7a4aa0
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-01 11:32:48 +0530

    complete job card setup with firebase database

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import Header from "./components/Header/Header.jsx"
-import Navbar from "./components/Navbar/Navbar.jsx"
-import SearchBar from "./components/SearchBar/SearchBar.jsx"
+import { useEffect, useState } from "react";
+import Header from "./components/Header/Header.jsx";
+import JobCard from "./components/JobCard/JobCard.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import SearchBar from "./components/SearchBar/SearchBar.jsx";
+//import jobData from "/JobData.js";
+import { collection, query,orderBy, getDocs } from "firebase/firestore";
+import { db } from "../firebase.config.js";
 
 const App = () => {
-  return (
-   <div>
-    <Navbar/>
-    <Header/>
-    <SearchBar/>
-   </div>
-  )
-}
+  const [jobs, setJobs] = useState([]);
 
-export default App
+  const fetchJobs = async () => {
+    const tempJobs = [];
+    const jobsRef = query(collection(db, "jobs"));
+    const q = query(jobsRef, orderBy("postDate", "desc"));
+    const req = await getDocs(q);
+
+
+    req.forEach((job) => {
+      //console.log(job.id, "=>", job.data());
+      tempJobs.push({
+        ...job.data(),
+        id: job.id,
+        postDate: job.data().postDate.toDate(),
+      });
+    });
+
+    setJobs(tempJobs);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <SearchBar />
+      {jobs.map((job) => {
+        return <JobCard key={job.id} {...job} />;
+      })}
+    </div>
+  );
+};
+
+export default App;
